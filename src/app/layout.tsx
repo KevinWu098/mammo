02e59d1f85e5d105a4bc3a02b7f853ff37cd0527
TimTimich6 <timofeyobraztsov@gmail.com>
2024-01-20 15:24:06 -0800

    feat: added pagesidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,10 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 
+import PageSidebarSlot from "@/components/PageSidebarSlot";
 import Providers from "@/components/Providers";
 import { cn } from "@/lib/utils";
+import { FileSearch2, Home, Scan, Zap } from "lucide-react";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -27,7 +29,25 @@ export default function RootLayout({
             <Providers>
                 <body className={cn(inter.className)}>
                     <main className="h-screen flex w-full">
-                        <div className="w-52 bg-white"> </div>
+                        <div className="w-52 bg-white flex-col py-64 justify-center">
+                            <PageSidebarSlot
+                                title="home"
+                                icon={<Home fill="white" strokeWidth={0} />}
+                                selected={true}
+                            ></PageSidebarSlot>
+                            <PageSidebarSlot
+                                title="scan"
+                                icon={
+                                    <FileSearch2 fill="white" strokeWidth={0} />
+                                }
+                                selected={false}
+                            ></PageSidebarSlot>
+                            <PageSidebarSlot
+                                title="act"
+                                icon={<Zap fill="white" strokeWidth={0} />}
+                                selected={false}
+                            ></PageSidebarSlot>
+                        </div>
                         <div className="flex-1 p-8">{children}</div>
                     </main>
                 </body>
---
+++ src/components/PageSidebarSlot.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface PageSidebarSlotProps {
+    selected: boolean;
+    title: string;
+    icon: React.ReactNode;
+}
+function PageSidebarSlot(props: PageSidebarSlotProps) {
+    return (
+        <div className="w-full">
+            <div className="bg-jas-grey-light rounded-2xl m-4 py-2 px-4 w-full flex gap-2 items-center">
+                <div className="bg-jas p-2 rounded-lg">{props.icon}</div>
+                <h1 className="text-2xl font-bold">{props.title}</h1>
+            </div>
+        </div>
+    );
+}
+
+export default PageSidebarSlot;
