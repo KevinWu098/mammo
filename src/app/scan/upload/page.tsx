fac2ea12e1886c61fbe6439644e1b90beeb17008
Kevin Wu <kevinwu098@gmail.com>
2024-01-21 09:37:22 -0800

    fix: boxes render

--- src/app/scan/upload/page.tsx
+++ src/app/scan/upload/page.tsx
@@ -9,6 +9,7 @@ import React, {
 } from "react";
 import Loading from "../load/loading";
 import ActionCard from "@/components/ActionCard";
+import Analysis from "@/components/Analysis";
 import IconizedBadge from "@/components/IconizedBadge";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
@@ -31,7 +32,6 @@ const Page = () => {
   const [submitted, setSubmitted] = useState(false);
   const [segmentedImage, setSegmentedImage] = useState<string>();
   const [coordinates, setCoordinates] = useState([]);
-  const segmentedImageRef = React.useRef<HTMLImageElement>(null);
   const onDrop = useCallback((acceptedFiles: File[]) => {
     setFile(acceptedFiles);
   }, []);
@@ -74,25 +74,6 @@ const Page = () => {
           console.log(error.message);
         });
     };
-
-    // // get the response
-    // const resJSON = await res.json();
-    // const segImg = resJSON.image;
-    // // decode it from base64
-    // const decodedData = atob(segImg);
-
-    // const arrayBuffer = new ArrayBuffer(decodedData.length);
-    // const uint8Array = new Uint8Array(arrayBuffer);
-
-    // for (let i = 0; i < decodedData.length; i++) {
-    //   uint8Array[i] = decodedData.charCodeAt(i);
-    // }
-    // const blob = new Blob([uint8Array], { type: "image/png" });
-
-    // // create the object url to display
-    // const objectURL = URL.createObjectURL(blob);
-    // setSegmentedImage(objectURL);
-
     reader.readAsDataURL(fileUpload);
   };
 
@@ -100,93 +81,7 @@ const Page = () => {
     return submitted && !segmentedImage ? (
       <Loading fileSize={30} />
     ) : (
-      <div className="flex  gap-8 p-8 items-center h-full justify-center relative bg-jas-grey_light  rounded-[2rem]">
-        <div className="flex flex-1 flex-col items-center gap-4 relative w-full h-full relative">
-          <h1 className="text-4xl font-extrabold w-full r">Your Scan</h1>
-          <Card className="flex w-full h-full rounded-3xl relative">
-            <>
-              {segmentedImage ? (
-                <img
-                  src={segmentedImage}
-                  alt="test"
-                  className="rounded-3xl p-1"
-                  ref={segmentedImageRef}
-                />
-              ) : null}
-              {segmentedImage &&
-                segmentedImageRef?.current &&
-                coordinates.map((coordinate: any) => {
-                  console.log(segmentedImageRef.current!.width);
-                  return (
-                    <div
-                      style={{
-                        width:
-                          (coordinate.width / 1024) *
-                          segmentedImageRef.current!.width,
-                        height:
-                          (coordinate.height / 1024) *
-                          segmentedImageRef.current!.height,
-                        position: "absolute",
-                        top:
-                          (coordinate.y / 1024) *
-                            segmentedImageRef.current!.height -
-                          coordinate.height / 4,
-                        left:
-                          (coordinate.x / 1024) *
-                            segmentedImageRef.current!.width -
-                          coordinate.width / 4,
-
-                        border: "2px solid red",
-                      }}
-                    ></div>
-                  );
-                })}
-            </>
-          </Card>
-
-          <Card className=" flex flex-col p-8 rounded-3xl w-full justify-evenly gap-2">
-            <div className="flex gap-2 items-center">
-              <div className="p-2 bg-red-600 rounded-xl">
-                <AlertTriangle fill="white" color="red" size={32} />
-              </div>
-              <h1 className="text-2xl font-bold">Note</h1>
-            </div>
-            <h2 className="text-xl font-bold text-jas-grey">
-              This is not a professional medical report; please consult a
-              professional.
-            </h2>
-          </Card>
-        </div>
-
-        <div className="flex flex-1 flex-col gap-4 items-center h-full">
-          <Card className="flex flex-col p-8 rounded-3xl gap-4 ">
-            <div className="w-max-[10rem]">
-              <IconizedBadge icon={<ScrollText />} title="your result" />
-            </div>
-            <div className="flex gap-2 items-center">
-              <h1 className="text-5xl font-extrabold">Severe</h1>
-              <h2 className="p-2 text-3xl font-bold bg-jas-red rounded-xl text-white">
-                88
-              </h2>
-            </div>
-
-            <h3 className="text-xl font-bold text-jas-grey">
-              Your results indicates that your health is at risk. Please consult
-              a professional.
-            </h3>
-          </Card>
-          <h1 className="text-4xl font-extrabold w-full">Take Action</h1>
-          <div className="flex flex-col gap-2 w-full">
-            <ActionCard
-              props={{
-                title: "Visit a local doctor",
-                Tag: <LocationTag />,
-                link: "https://google.com",
-              }}
-            />
-          </div>
-        </div>
-      </div>
+      <Analysis image={segmentedImage} coordinates={coordinates} />
     );
   }
 
