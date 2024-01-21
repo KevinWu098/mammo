bf279511e7e25dee0988bb93923f9621884d903a
TimTimich6 <timofeyobraztsov@gmail.com>
2024-01-20 18:17:35 -0800

    actions page for jas

---
+++ src/components/ActionGroupCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ActionGroupCardProps {
+    checked: boolean;
+    title: string;
+}
+export default function ActionGroupCard() {
+    return <div>ActionGroupCard</div>;
+}
